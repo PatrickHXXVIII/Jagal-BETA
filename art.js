0cad0e3710a935a5e419245726bc708783de719f
Hirsch <patrick.hirsch@stu.boone.kyschools.us>
2019-01-28 13:14:54 -0500

    Update

---
+++ art.js
@@ -0,0 +1,9 @@
+function setScene1()
+{	document.getElementById("Scene2").style.display = "none";
+	document.getElementById("Scene1").style.display = "block";}
+
+function setScene2()
+{	document.getElementById("Scene1").style.display = "none";
+	document.getElementById("Scene2").style.display = "block";}
+
+setScene1();
