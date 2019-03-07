f03a9531eaeeac0857299edd72412a33d051aacf
Patrick Hirsch <39008177+PatrickHXXVIII@users.noreply.github.com>
2019-03-07 13:21:49 -0500

    Error messages

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var timeString = Date().toString() + " " + Date.now();
 var URLroot = "https://maker.ifttt.com/trigger/jagallery_vote/with/key/kAL7tTNl_NMHU_aaPHT-Uijak39EddbxriyONMYnFOW?value3=" + timeString;
+var radios = document.getElementsByName('value2');
+
+function check()
+{	for (var i = 0, length = radios.length; i < length; i++) {if (radios[i].checked){var rad = radios[i].value; break;}}
+	if(document.getElementById('value1').value == "")
+	{	alert("Please enter your name!");}
+	else
+	{	send();}}
 
 function send()
-{	var radios = document.getElementsByName('value2');
-	for (var i = 0, length = radios.length; i < length; i++) {if (radios[i].checked){var vote = radios[i].value; break;}}
+{	for (var i = 0, length = radios.length; i < length; i++) {if (radios[i].checked){var vote = radios[i].value; break;}}
 	var voter = document.getElementById('value1').value;
 	var URL = URLroot+"&value1="+voter+"&value2="+vote;
 	alert(URL);
